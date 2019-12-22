07c36637d4bb8aac462ec2aff054af852c7bf2fb
Dries Bos <info@driesbos.com>
2019-12-22 13:48:41 +0100

    markdown component

---
+++ components/MarkdownItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="markdown" v-html="compiledMarkdown"></div>
+</template>
+
+<script>
+// www.https://marked.js.org/
+const marked = require("marked")
+const renderer = new marked.Renderer()
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  breaks: true
+})
+
+export default {
+  name: "MarkdownItem",
+  props: {
+    input: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    compiledMarkdown: function() {
+      return marked(this.input)
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+// Prefer style handling by cascading css (typography.sass) as much as possible.
+</style>
