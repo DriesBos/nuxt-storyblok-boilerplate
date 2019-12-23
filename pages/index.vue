c6742c507654307f6918fce8a68abad1de3c9cd5
Dries Bos <info@driesbos.com>
2019-12-23 17:17:21 +0100

    dotenv

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,24 @@
 <template>
   <section>
     <picture>
-      <source
-        :srcset="require('~/assets/images/landscape-1.jpg?webp')"
-        type="image/webp"
-      />
-      <source
-        :srcset="require('~/assets/images/landscape-1.jpg')"
-        type="image/jpeg"
-      />
+      <source :srcset="require('~/assets/images/landscape-1.jpg?webp')" type="image/webp" />
+      <source :srcset="require('~/assets/images/landscape-1.jpg')" type="image/jpeg" />
       <img :src="require('~/assets/images/landscape-1.jpg')" />
     </picture>
   </section>
 </template>
 
-<script lang="ts">
-import Vue from "vue"
-export default Vue.extend({})
+<script>
+export default {
+  methods: {
+    testingEnvKeys() {
+      console.log(process.env.TEST)
+    }
+  },
+  mounted() {
+    this.testingEnvKeys()
+  }
+}
 </script>
 
 <style></style>
