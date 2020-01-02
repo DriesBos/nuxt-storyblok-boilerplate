aa560859bd6062ea9902dbcfb24b531d2bb9af62
Dries Bos <info@driesbos.com>
2020-01-02 16:05:09 +0100

    about page

---
+++ pages/about/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <p>ABOUT</p>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.testingEnvKeys()
+  },
+  methods: {
+    testingEnvKeys() {
+      console.log(process.env.TEST)
+    }
+  }
+}
+</script>
+
+<style></style>
