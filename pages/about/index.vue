46c8678c203df5a6b82ade7e46c1905b00f829c2
Dries Bos <info@driesbos.com>
2020-01-02 17:33:23 +0100

    fetching Storyblok aSyncData

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,15 +1,49 @@
 <template>
   <section>
-    <p>ABOUT</p>
+    <h1>{{ this.story.content.title }}</h1>
+    <br />
+    <markdown-item :input="this.story.content.text" />
   </section>
 </template>
 
 <script>
 import storyblokLivePreview from "@/mixins/storyblokLivePreview"
+import MarkdownItem from "@/components/MarkdownItem.vue"
 
 export default {
-  mixins: [storyblokLivePreview]
+  components: {
+    "markdown-item": MarkdownItem
+  },
+  mixins: [storyblokLivePreview],
+  asyncData(context) {
+    return context.app.$storyapi
+      .get("cdn/stories/about", {
+        version: "draft"
+      })
+      .then(res => {
+        return res.data
+      })
+      .catch(res => {
+        if (!res.response) {
+          console.error(res)
+          context.error({
+            statusCode: 404,
+            message: "Failed to receive content form api"
+          })
+        } else {
+          console.error(res.response.data)
+          context.error({
+            statusCode: res.response.status,
+            message: res.response.data
+          })
+        }
+      })
+  },
+  data() {
+    return {
+      story: { content: {} }
+    }
+  },
+  mounted() {}
 }
 </script>
-
-<style></style>
