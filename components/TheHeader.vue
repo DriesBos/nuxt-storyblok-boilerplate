3e8a46e3774b45670c54d8a49c4bf445f08d8e2d
Dries Bos <info@driesbos.com>
2020-01-02 15:32:35 +0100

    TheHeader

---
+++ components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <nuxt-link to="/" tag="li">Home</nuxt-link>
+        <nuxt-link to="/for-brands" tag="li">For Brands</nuxt-link>
+        <nuxt-link to="/for-art" tag="li">For Art</nuxt-link>
+        <nuxt-link to="/for-people" tag="li">For People</nuxt-link>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TheHeader"
+}
+</script>
