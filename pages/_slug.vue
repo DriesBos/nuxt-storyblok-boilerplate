e8de0995b0c0587e832bbe30cf7e2c23cf5a8a84
Dries Bos <info@driesbos.com>
2020-01-20 16:37:10 +0000

    testing dynamic route

---
+++ pages/_slug.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <component
+      v-if="story.content.component"
+      :key="story.content._uid"
+      :blok="story.content"
+      :is="story.content.component | dashify"
+    ></component>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return { story: { content: {} } }
+  },
+  asyncData(context) {
+    // Check if we are in the editor mode
+    let version =
+      context.query._storyblok || context.isDev ? "draft" : "published"
+
+    // Load the JSON from the API
+    return context.app.$storyapi
+      .get(`cdn/stories/${context.params.slug}`, {
+        version: version
+      })
+      .then(res => {
+        return res.data
+      })
+      .catch(res => {
+        context.error({
+          statusCode: res.response.status,
+          message: res.response.data
+        })
+      })
+  }
+}
+</script>
