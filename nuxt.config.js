98af3937c276d3963d4ae29a868113102dacaca3
Dries Bos <info@driesbos.com>
2020-01-20 16:14:15 +0000

    added dotenv vars

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,11 +51,10 @@ module.exports = {
     [
       "storyblok-nuxt",
       {
-        accessToken: "MrMcVxCit5lL9WS95csYUAtt",
+        accessToken: process.env.PREVIEWKEY,
         cacheProvider: "memory"
       }
-    ],
-    ["@nuxtjs/dotenv"]
+    ]
   ],
   /*
    ** Build configuration
