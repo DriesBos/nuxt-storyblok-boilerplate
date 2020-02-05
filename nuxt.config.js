fe477c0cc69cf313f2d9f3732515acd2f6fa94d5
Dries Bos <info@driesbos.com>
2020-02-05 11:44:13 +0000

    meta to cover notches

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,10 @@ module.exports = {
     title: pkg.name,
     meta: [
       { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1, viewport-fit=cover"
+      },
       { hid: "description", name: "description", content: pkg.description }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
