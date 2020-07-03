5562cc79f832ac47503bd85bcfa435e9c021a506
Dries Bos <info@driesbos.com>
2020-07-03 13:41:29 +0200

    meta update

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,17 +9,17 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: pkg.name, // Change in package.json files
     meta: [
       { charset: "utf-8" },
       {
         name: "viewport",
-        content: "width=device-width, initial-scale=1, viewport-fit=cover"
+        content: "width=device-width, initial-scale=1"
       },
       {
         hid: "description",
         name: "description",
-        content: pkg.description
+        content: pkg.description // Change in package.json files
       },
       {
         name: "mobile-web-app-capable",
