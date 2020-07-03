76fc6ab871f60c6c44ea390f296ce6878faa8697
Dries Bos <info@driesbos.com>
2020-07-03 14:54:58 +0200

    cleanup

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -26,7 +26,7 @@ export default {
           console.error(res)
           context.error({
             statusCode: 404,
-            message: "Failed to receive content form api"
+            message: "Failed to receive content from api"
           })
         } else {
           console.error(res.response.data)
