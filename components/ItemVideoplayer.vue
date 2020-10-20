ca97ca76a538ddf01c7130d3d5f57deb10f34dcf
Dries Bos <driesbos@MBP-van-Dries.home>
2020-10-20 12:48:27 +0200

    style: video comp

--- components/ItemVideoplayer.vue
+++ components/ItemVideoplayer.vue
@@ -2,14 +2,14 @@
   <div v-editable="blok" class="item item-Videoplayer">
     <div class="item-Videoplayer_Outer" :class="blok.width">
       <div :id="blok._uid" class="item-Videoplayer_Outer">
-        <player-youtube
+        <LazyPlayerYoutube
           v-if="blok.youtube_id != ''"
           :videoid="blok.youtube_id"
           autoplay="1&mute=1"
           title=""
           class="item-Videoplayer_Youtube"
         />
-        <player-vimeo
+        <LazyPlayerVimeo
           v-if="blok.vimeo_id != ''"
           :videoid="blok.vimeo_id"
           autoplay="1&mute=1"
@@ -35,6 +35,17 @@ export default {
 <style lang="sass">
 .item
   &-Videoplayer
+    &_Outer
+      position: relative
+      padding-bottom: 56.25% //16:9
+      height: 0
+    video
+      width: 100% !important
+      height: auto !important
     iframe
+      position: absolute
+      top: 0
+      left: 0
       width: 100%
+      height: 100%
 </style>
