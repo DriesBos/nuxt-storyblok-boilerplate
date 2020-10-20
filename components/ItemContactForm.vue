4e96821f45e690b381cd3bb2375460358efe016a
Dries Bos <driesbos@MBP-van-Dries.home>
2020-10-20 12:06:26 +0200

    added contact form item

---
+++ components/ItemContactForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <section v-editable="blok" class="item item-ContactForm">
+    <!-- prettier-ignore -->
+    <form
+      name="contact"
+      method="post"
+      action="/success"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <input type="text" required name="name" placeholder="name" />
+      <input type="text" required name="email" placeholder="email" />
+      <input type="text" required name="message" placeholder="message" />
+      <div class="subSection-Contact_Send">
+        <input type="submit" value="submit" title="submit"/>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: Object
+  }
+}
+</script>
+
+<style lang="sass">
+.item
+  &-ContactForm
+    form
+      display: flex
+      flex-direction: column
+      input
+        padding: 1rem
+        font-size: 1rem
+        border: 0
+        border-bottom: 1px solid $support-color
+        background: transparent
+        width: 100%
+        &::placeholder
+          color: $color
+</style>
--- plugins/components.js
+++ plugins/components.js
@@ -5,6 +5,7 @@ import Page from "~/components/Page.vue"
 import PagePost from "~/components/PagePost.vue"
 
 // ITEMS
+import ItemContactForm from "~/components/ItemContactForm.vue"
 import ItemImages from "~/components/ItemImages.vue"
 import ItemText from "~/components/ItemText.vue"
 
@@ -13,5 +14,6 @@ Vue.component("blok-page", Page)
 Vue.component("blok-page-post", PagePost)
 
 // ITEMS
+Vue.component("blok-item-contact-form", ItemContactForm)
 Vue.component("blok-item-images", ItemImages)
 Vue.component("blok-item-text", ItemText)
