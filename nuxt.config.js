8fa1c0ad17aa425525dfee00a4d97cbad434a808
Dries Bos <driesbos@MBP-van-Dries.home>
2020-10-30 09:45:02 +0100

    meta: viewport-fit = true

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
       { charset: "utf-8" },
       {
         name: "viewport",
-        content: "width=device-width, initial-scale=1"
+        content: "width=device-width, initial-scale=1, viewport-fit=cover"
       },
       {
         hid: "description",
