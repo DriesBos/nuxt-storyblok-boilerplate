d3ddd6c370060c23efe541582e8e90569838bf1b
Dries Bos <driesbos@MBP-van-Dries.home>
2020-10-31 14:14:50 +0100

    update error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,13 @@
 <template>
-  <p>404 ERROR</p>
+  <div class="section-Wrapper">
+    <section class="section-Error">
+      <div class="section-Error_Content">
+        <p>
+          Oops! Page not found.<br />
+          Visit <nuxt-link to="/">home</nuxt-link> or see my
+          <nuxt-link to="/projects">projects</nuxt-link>.
+        </p>
+      </div>
+    </section>
+  </div>
 </template>
